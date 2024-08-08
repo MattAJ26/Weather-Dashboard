a7f48d949c2e35169eeae3a747e8337b609866ab
Matt Johnson <mattajohnson26@gmail.com>
2024-08-08 14:19:53 -0600

    got rid of unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,11 @@ function handleSearchFormSubmit(event) {
   event.preventDefault();
 
   const searchInputVal = document.querySelector('#search-input').value;
-  //const formatInputVal = document.querySelector('#format-input').value;
 
   if (!searchInputVal) {
     console.error('You need a search input value!');
     return;
   }
-
- // const queryString = `./search-results.html?q=${searchInputVal}&format=${formatInputVal}`;
-
- // location.assign(queryString);
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
